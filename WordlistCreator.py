b21aa0dca9c159d015ce8421c9cddfb5c373d001
espfre <espfre@gmail.com>
2015-11-05 16:27:00 +0100

    Changed filenames and made the function names more consistent

--- WordlistCreator.py
+++ WordlistCreator.py
@@ -1,9 +1,7 @@
 import random
-
-alf = {'a': 'alpha', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo', 'f': 'foxtrot', 'g': 'golf', 'h': 'hotel',
-       'i': 'india', 'j': 'juliet', 'k': 'kilo', 'l': 'lima', 'm': 'mike', 'n': 'november', 'o': 'oscar', 'p': 'papa',
-       'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango', 'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x':
-       'x-ray', 'y': 'yankee', 'z': 'zulu'}
+import PhoneticAlphabet
+alphabet = PhoneticAlphabet.PhoneticAlphabet()
+alf = alphabet.alf()
 
 
 class CreateWordList:
@@ -20,11 +18,11 @@ class CreateWordList:
         content.close()
         return self.lines
 
-    def word(self):                                      # Gets a random word for the test
-        word = self.lines[random.randrange(0, 10000)].rstrip("\n")
+    def word(self):
+        word = self.lines[random.randrange(0, 100000)].rstrip("\n")
         return word
 
-    def word_list(self, word):                            # Makes a list of the chosen word
+    def wordList(self, word):
         print ("")
         word_list = []
         for letter in word:
